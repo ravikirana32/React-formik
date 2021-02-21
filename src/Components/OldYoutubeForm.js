a8d5cb749d6e6cdfb8748887e9cf1e64bc797647
ravikirana32 <ravikirana32@gmail.com>
2021-02-21 15:19:32 +0530

    From tag from formik used

--- src/Components/OldYoutubeForm.js
+++ src/Components/OldYoutubeForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useFormik} from 'formik';
+import {Formik,Form} from 'formik';
 import * as yup from 'yup';
 
 const OldYoutubeForm=()=> {
@@ -25,38 +25,38 @@ const OldYoutubeForm=()=> {
                 .required("Channel Required")
     })
 
-    const formik = useFormik({
-        initialValues:initialValues,
-        onSubmit:onSubmit,
-        validationSchema:validationSchema
-    });
-    //console.log("is touched ",formik.touched);
     return (
         <div>
-            <form onSubmit={formik.handleSubmit}>
-                <label htmlFor="name">Name</label>
-                <input 
-                    type="text" 
-                    id="name" 
-                    name="name" 
-                    {...formik.getFieldProps('name')}/>
-                {(formik.errors.name && formik.touched.name) && <p style={{color:'red',display:'block'}}>{formik.errors.name}</p>}
-                <label htmlFor="email">Email</label>
-                <input 
-                    type="email" 
-                    id="email" 
-                    name="email" 
-                    {...formik.getFieldProps('email')}/>
-                {(formik.errors.email && formik.touched.email) && <p style={{color:'red',display:'block'}}>{formik.errors.email}</p>}
-                <label htmlFor="channel">Channel</label>
-                <input 
-                    type="text" 
-                    id="channel" 
-                    name="channel" 
-                    {...formik.getFieldProps('channel')}/>
-                {(formik.errors.channel && formik.touched.channel) && <p style={{color:'red',display:'block'}}>{formik.errors.channel}</p>}
-                <button type="submit">Create Channel</button>
-            </form>
+            <Formik
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={onSubmit}
+            >
+                <Form>
+                    <label htmlFor="name">Name</label>
+                    <input 
+                        type="text" 
+                        id="name" 
+                        name="name" 
+                        {...formik.getFieldProps('name')}/>
+                    {(formik.errors.name && formik.touched.name) && <p style={{color:'red',display:'block'}}>{formik.errors.name}</p>}
+                    <label htmlFor="email">Email</label>
+                    <input 
+                        type="email" 
+                        id="email" 
+                        name="email" 
+                        {...formik.getFieldProps('email')}/>
+                    {(formik.errors.email && formik.touched.email) && <p style={{color:'red',display:'block'}}>{formik.errors.email}</p>}
+                    <label htmlFor="channel">Channel</label>
+                    <input 
+                        type="text" 
+                        id="channel" 
+                        name="channel" 
+                        {...formik.getFieldProps('channel')}/>
+                    {(formik.errors.channel && formik.touched.channel) && <p style={{color:'red',display:'block'}}>{formik.errors.channel}</p>}
+                    <button type="submit">Create Channel</button>
+                </Form>
+            </Formik>
         </div>
     )
 }
