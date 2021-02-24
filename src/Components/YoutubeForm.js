a717f47ad49a3a38f1ed7339cd6b8342d743e5de
ravikirana32 <ravikirana32@gmail.com>
2021-02-24 15:28:35 +0530

    different form of input types added

--- src/Components/YoutubeForm.js
+++ src/Components/YoutubeForm.js
@@ -8,7 +8,9 @@ const YoutubeForm=()=> {
     const initialValues={
         name:'',
         email:'',
-        channel:''
+        channel:'',
+        comments:'',
+        address:''
     };
     const onSubmit=values=>{
         console.log("Values ",values);
@@ -24,7 +26,13 @@ const YoutubeForm=()=> {
                 .required("Email required"),
         channel:yup
                 .string()
-                .required("Channel Required")
+                .required("Channel Required"),
+        comments:yup
+                .string()
+                .required("Comments Required"),
+        address:yup
+                .string()
+                .required("Address Required")
     })
 
     return (
@@ -39,20 +47,46 @@ const YoutubeForm=()=> {
                     <Field 
                         type="text" 
                         id="name" 
-                        name="name" />
+                        name="name" 
+                        placeholder="Name"/>
                     <ErrorMessage name='name' />
                     <label htmlFor="email">Email</label>
                     <Field 
                         type="email" 
                         id="email" 
-                        name="email" />
+                        name="email"
+                        placeholder="Email" />
                     <ErrorMessage name='email' />
                     <label htmlFor="channel">Channel</label>
                     <Field 
                         type="text" 
                         id="channel" 
-                        name="channel" />
+                        name="channel"
+                        placeholder="Youtube Channel Name" />
                     <ErrorMessage name='channel' />
+                    <div className="form-control">
+                    <label htmlFor="comments">Comments</label>
+                        <Field 
+                            as='textarea'//A
+                            //component="textarea"//B  both A and  B are same, B is depricated
+                            id="comments" 
+                            name="comments"
+                            placeholder="Youtube comments" />
+                            <ErrorMessage name='comments' />
+                    </div>
+                    <div className="form-control">
+                    <label htmlFor="address">Address</label>
+                        <Field 
+                            name="address" >
+                                {
+                                    (props)=>{
+                                        const {field,meta,form}=props;
+                                        console.log(props);
+                                        return <><input id="address" type="text" {...field}/>{meta.touched && meta.error?<p>{meta.error}</p>:null}</>
+                                    }
+                                }
+                        </Field>
+                    </div>
                     <button type="submit">Create Channel</button>
                 </Form>
             </Formik>
